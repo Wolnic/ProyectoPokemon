7535489cae9001453f723975ac846c7e526be0c9
JosuCH16 <gjchernandez6@gmail.com>
2025-03-21 22:28:55 -0600

    prueba de cambioi en entrenador

---
+++ Desktop/ADATA/FIDELITAS/06- CUATRI/Estructura de Datos/Proyecto/ProyectoPokemon-main/ProyectoEdDPokemon/src/main/java/com/mycompany/proyectoeddpokemon/ProyectoPokemon.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.proyectoeddpokemon;
+
+/**
+ * Proyecto Estructuras De Datos Pokemon
+ *
+ * @profesor Pablo Esteban Gomez Ramirez
+ *
+ * @author  *
+ * Ocampo Pantoja José Mario  *
+ * Sánchez Venegas Manuel Antonio  *
+ * Delgado Jiménez Alexander  *
+ * Cordoba Hernandez Giovanny Josue  *
+ * Pérez Rodríguez Carlos Roberto  *
+ */
+public class ProyectoPokemon {
+
+    public static void main(String[] args) {
+        int CodigoPokemon = 1;
+
+    }
+}
